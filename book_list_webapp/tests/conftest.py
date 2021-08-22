d2b2d7445037495bdb728aa116f2dfc622b71589
MichavR <michalrdz@gmail.com>
2021-08-22 19:29:33 +0200

    app testing preparations

---
+++ book_list_webapp/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from django.core.management import call_command
+from django.test import Client
+
+
+# this fixture prepopulates database with test objects from test_data.json
+@pytest.fixture(scope="session")
+def db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command("loaddata", "data/test_data.json")
+
+
+@pytest.fixture
+def client():
+    client = Client()
+    return client
