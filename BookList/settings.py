ce338fa42a876704a9743621dcd31781a65d49c7
MichavR <michalrdz@gmail.com>
2021-08-25 01:20:08 +0200

    fixing heroku deploy problems

--- BookList/settings.py
+++ BookList/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [".heroku.com", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
