b1c177cbc9a9d3b8e3bd8a275476b83887922b51
MichavR <michalrdz@gmail.com>
2021-08-26 21:44:51 +0200

    model fields refactor

--- book_list_webapp/models.py
+++ book_list_webapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Books(models.Model):
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
-    publication_date = models.CharField(max_length=10, null=True)
+    publication_date = models.DateField(null=True, blank=True)
     ISBN = models.CharField(max_length=13)
     page_count = models.IntegerField(null=True)
-    cover_link = models.CharField(max_length=512, null=True, blank=True)
+    cover_link = models.URLField(null=True)
     publication_language = models.CharField(max_length=2, null=True)
